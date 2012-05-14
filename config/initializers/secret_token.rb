8e9d5029bc380891aab9dbe0051626fc6d91a011
Kashif Saeed <kashif@kashif.(none)>
2012-05-14 13:26:13 +0500

    adding project into the github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRuby::Application.config.secret_token = '4a180ff06c67ea3070f662f2bfe07311c5bb4329af978283aabb450bd2f725a1baa5cf403ff39f5713647cc9ccf5f01afac7486c443b71d02bb04a6b57e62a52'
